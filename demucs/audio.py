3f6dc33c1d9bd865ca7c9ea5c3aa89b80745214c
NotNANtoN <antonwiehe@gmail.com>
2023-07-14 13:40:52 +0200

    Update audio.py

--- demucs/audio.py
+++ demucs/audio.py
@@ -3,6 +3,7 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+import os
 import json
 import subprocess as sp
 from pathlib import Path
