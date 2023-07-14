8c96afdc2ad1a5bb88847a0959236dcd51716a9c
NotNANtoN <antonwiehe@gmail.com>
2023-07-14 14:08:15 +0200

    Update audio.py

--- demucs/audio.py
+++ demucs/audio.py
@@ -252,8 +252,9 @@ def save_audio(wav: torch.Tensor,
     if suffix == ".mp3":
         # encode_mp3(wav, path, samplerate, bitrate, verbose=True)
         path = str(path)
+        print("Saving MP3 to: ", path)
         os.makedirs("/".join(path.split("/")[:-1]), exist_ok=True)
-        sf.write(path, wav.float().cpu().numpy(), samplerate=samplerate, format="MP3")
+        sf.write(path, wav.squeeze().float().cpu().numpy(), samplerate=samplerate, format="MP3")
     elif suffix == ".wav":
         if as_float:
             bits_per_sample = 32
